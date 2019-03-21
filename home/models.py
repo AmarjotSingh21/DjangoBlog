e4428944f06b42d5fb5d2e72fe4592abc845666c
Prince <alsoprince@gmail.com>
2019-03-21 06:19:41 +0530

    typography settings

--- home/models.py
+++ home/models.py
@@ -28,7 +28,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    comment_body = models.TextField()
+    comment_body = models.TextField(blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     date_posted = models.DateTimeField(auto_now_add=True)
