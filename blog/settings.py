ddeb265c3895117080846471422bc7ca5bef9457
Prince <alsoprince@gmail.com>
2019-03-22 13:34:42 +0530

    Final

--- blog/settings.py
+++ blog/settings.py
@@ -141,16 +141,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = f'Blog <{EMAIL_HOST_USER}>'
-
-
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_SIGNATURE_VERSION = "s3v4"
-AWS_S3_REGION_NAME = "ap-south-1"
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-django_heroku.settings(locals())
+DEFAULT_FROM_EMAIL = f'Programming Blog <{EMAIL_HOST_USER}>'
